7ca91ea338348cf30b50b8be5da629ab7d384843
Myfirstgit <mishraharshit60mail.com>
2024-11-28 13:22:55 +0530

    Updates

---
+++ precache-manifest.49a4f5883c40c80302555d20f5d1b9ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b603ef075bd4847f35ca964524d38356",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "23ed6053020883c09594",
+    "url": "/kanban-board/static/css/main.b5b68e72.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/kanban-board/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/kanban-board/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "23ed6053020883c09594",
+    "url": "/kanban-board/static/js/main.2ebdf2a4.chunk.js"
+  },
+  {
+    "revision": "a6709c8c3d5459931c7e",
+    "url": "/kanban-board/static/js/runtime~main.429370e3.js"
+  }
+]);
